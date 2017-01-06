b3901b2f28bb26d59601e925a3c1c1eb43c6f761
Anders Hafreager <andershaf@gmail.com>
2017-01-06 14:25:01 +0100

    Fixed qrc, added images

--- generateExamples.py
+++ generateExamples.py
@@ -4,7 +4,7 @@ import os
 from simulation import Simulation
 
 singlewater = Simulation(name="singlewater", inputScript="singlewater.in", tags = ["water", "manybody", "vashishta"], folder="examples/water/singlewater", title="Single water molecule", description = "A single water molecule using the vashishta potential.")
-watervapor = Simulation(name="watervapor", inputScript="watervapor.in", tags = ["water", "manybody", "vashishta"], folder="examples/water/watervapor", title="Water vapor", description = "Low density water gas using the vashishta potential.")
+watervapor = Simulation(name="watervapor", inputScript="watervapor.in", tags = ["water", "manybody", "vashishta"], folder="examples/water/vapor", title="Water vapor", description = "Low density water gas using the vashishta potential.")
 betacristobalite = Simulation(name="betacristobalite", inputScript="betacristobalite.in", tags = ["silica", "crystal", "manybody", "vashishta"], folder="examples/silica/betacristobalite", title="Silica beta cristobalite", description = "Beta cristobalite crystal using the vashishta potential.")
 zeolite_zsm5 = Simulation(name="zeolite_zsm5", inputScript="zeolite_zsm5.in", tags = ["silica", "zeolite", "manybody", "vashishta"], folder="examples/silica/zeolite_zsm5", title="Zeolite ZSM-5", description = "Zeolite ZSM-5 using the vashishta potential.")
 generate_nanoporous = Simulation(name="generate_nanoporous", inputScript="generate_nanoporous.in", tags = ["silica", "manybody", "vashishta"], folder="examples/silica/generate_nanoporous", title="Nanoporous silica", description = "Generate a nanoporous silica structure from betacristobalite using the vashishta potential.")
@@ -21,8 +21,12 @@ examplesDictList = []
 fileList = []
 
 for example in examples:
+    print "Working with ", example.name
+    print "Folder: ", example.folder
+    print "Files: ", example.files
     examplesDictList.append(example.__dict__)
-    fileList.extend(example.files)
+    for file in example.files:
+        fileList.append(os.path.join(example.folder, file))
 
 stringified = json.dumps(examplesDictList)
 print stringified
@@ -33,5 +37,6 @@ with open('examples.json', 'w') as outfile:
 with open('examples.qrc', 'w') as outfile:
     outfile.write("<RCC>\n    <qresource prefix=\"/\">\n")
     for file in fileList:
-        outfile.write("        <file>images/%s</file>\n" % file)
+        outfile.write("        <file>%s</file>\n" % file)
+    outfile.write("        <file>examples.json</file>\n")
     outfile.write("    </qresource>\n</RCC>")
--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 
 ignoreList = [".DS_Store", "log.lammps"]
 class Simulation:
-	def __init__(self, name, inputScript, folder, title, description, tags, thumbnails = [], files = []):
+	def __init__(self, name, inputScript, folder, title, description, tags, thumbnails = None, files = None):
 		self.name = name
 		self.inputScript = inputScript
 		self.folder = folder
@@ -12,12 +12,20 @@ class Simulation:
 		self.description = description
 		self.tags = tags
 		self.thumbnails = thumbnails
-		if len(self.files) == 0:
+		print self.name
+		print "Files now: ", self.files
+		if self.thumbnails is None:
+			self.thumbnails = []
+		if self.files is None:
+			self.files = []
 			self.findFiles()
+		
 
 	def findFiles(self):
+		print "Finding files for folder: ", self.folder
 		for f in listdir(self.folder):
 			if isfile(join(self.folder, f)):
+				print "Checking files: ", f
 				if f in ignoreList: continue
 				if f.lower().endswith(('.png', '.jpg', '.jpeg')):
 					self.thumbnails.append(f)
